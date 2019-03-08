c67104a93f27db1367235a3a5c18f6255b978199
vikas <vikas.on@gmail.com>
2019-03-08 21:26:06 +0700

    restructure classes

--- src/main/java/com/example/springrestjwt/SpringRestJwtApplication.java
+++ src/main/java/com/example/springrestjwt/SpringRestJwtApplication.java
@@ -1,18 +1,11 @@
 package com.example.springrestjwt;
 
-import com.example.springrestjwt.controller.repo.UserRepository;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 
 @SpringBootApplication
 public class SpringRestJwtApplication {
-    private final UserRepository userRepository;
-
-    public SpringRestJwtApplication(UserRepository userRepository) {
-        this.userRepository = userRepository;
-    }
-
     public static void main(String[] args) {
         SpringApplication.run(SpringRestJwtApplication.class, args);
     }
--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -4,6 +4,6 @@ insert into user (id, first_name, last_name, password, username) values
   (2, 'james','james','$2a$04$P2GbxPDh1MYNYyNn/bj.4.QxwDC2jze0xPQF4u6/cNpdkrPq3OdPy', 'james');
 insert into role (id, description, role_name) values (1,  'admin role', 'ADMIN');
 insert into role (id, description, role_name) values (2, 'user role', 'USER');
-insert into user_role(user_id, role_id) values (1, 1);
+insert into user_role(user_id, role_id) values (1, 2);
 insert into user_role(user_id, role_id) values (2, 1);
 insert into user_role(user_id, role_id) values (2, 2);
